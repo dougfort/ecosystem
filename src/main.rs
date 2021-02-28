70d47e89f965faa45bcc59a1b8bbbfbe8aa32506
dougfort <doug.fort@gmail.com>
2021-02-27 19:47:21 -0500

    add part of client

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use futures::Stream;
+use futures_util::StreamExt;
 use std::env;
 use std::pin::Pin;
 use tokio::sync::mpsc;
@@ -22,9 +23,47 @@ impl Organism for OrganismService {
 
     async fn food_flow(
         &self,
-        _request: Request<tonic::Streaming<Food>>,
+        request: Request<tonic::Streaming<Food>>,
     ) -> Result<Response<Self::FoodFlowStream>, Status> {
-        unimplemented!()
+        tracing::info!("food_flow = {:?}", request);
+
+        let (tx, rx) = mpsc::channel(1);
+
+        tokio::spawn(async move {
+            let mut stream = request.into_inner();
+            while let Some(food) = stream.next().await {
+                let food = food.unwrap();
+                tracing::info!("incoming food = {:?}", food);
+            }
+        });
+
+        tokio::spawn(async move {
+            let interval_seconds: u64 = 2;
+            let interval_duration = time::Duration::from_secs(interval_seconds);
+            let mut interval = time::interval(interval_duration);
+            let mut food_id: usize = 0;
+
+            loop {
+                interval.tick().await;
+                food_id += 1;
+                let food = Food {
+                    id: food_id as i32,
+                    kind: 1,
+                    amount: 1,
+                };
+                tracing::info!("food = {:?}", food);
+                if let Err(e) = tx.send(Ok(food)).await {
+                    tracing::info!("tx.send failed: {}", e);
+                    break;
+                }
+            }
+
+            tracing::info!("exit interval loop")
+        });
+
+        Ok(Response::new(Box::pin(
+            tokio_stream::wrappers::ReceiverStream::new(rx),
+        )))
     }
 }
 
@@ -37,7 +76,41 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // "[::1]:10000"
     let args: Vec<String> = env::args().collect();
     let our_addr = &args[1].parse().unwrap();
-    // let peer_addr = &args[2].parse().unwrap();
+
+    let peer_addr = args[2].clone();
+    tokio::spawn(async move {
+        let uri = format!("http://{}", peer_addr);
+        let mut client = OrganismClient::connect(uri).await.expect("Connect failed");
+
+        let outbound = async_stream::stream! {
+            let interval_seconds: u64 = 2;
+            let interval_duration = time::Duration::from_secs(interval_seconds);
+            let mut interval = time::interval(interval_duration);
+            let mut food_id: usize = 0;
+
+            loop {
+                interval.tick().await;
+                food_id += 1;
+                let food = Food {
+                    id: food_id as i32,
+                    kind: 1,
+                    amount: 1,
+                };
+                tracing::info!("food = {:?}", food);
+                yield food;
+            }
+        };
+
+        let response = client
+            .food_flow(Request::new(outbound))
+            .await
+            .expect("food_flow failed");
+        let mut inbound = response.into_inner();
+
+        while let Some(food) = inbound.message().await.expect("nbound.message() failed") {
+            println!("food = {:?}", food);
+        }
+    });
 
     let organism_service = OrganismService;
     let svc = OrganismServer::new(organism_service);
