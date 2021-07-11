e35628f9ff3680f75908c17d8d40eba29fbca547
dougfort <doug.fort@gmail.com>
2021-07-11 18:06:37 -0400

    remove unused food is from State

--- src/bin/organism/state.rs
+++ src/bin/organism/state.rs
@@ -2,7 +2,6 @@ use crate::ecosystem::Food;
 
 #[derive(Debug)]
 pub struct State {
-    food_id: usize,
     outgoing: String,
     incoming: String,
 }
@@ -10,15 +9,12 @@ pub struct State {
 impl State {
     pub fn new(outgoing: &str, incoming: &str) -> Self {
         State {
-            food_id: 0,
             outgoing: outgoing.to_string(),
             incoming: incoming.to_string(),
         }
     }
 
     pub fn produce_food(&mut self) -> Food {
-        self.food_id += 1;
-
         Food {
             kind: self.outgoing.to_string(),
             amount: 1,
